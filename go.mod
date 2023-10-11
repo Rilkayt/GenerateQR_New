2e64fbefe17aca01a6025602c891130e54ab608c
Rilkayt <rilkayt@gmail.com>
2023-10-12 01:41:12 +0700

    commit-pertama

---
+++ go.mod
@@ -0,0 +1,10 @@
+module GenerateQR
+
+go 1.21.3
+
+require github.com/skip2/go-qrcode v0.0.0-20200617195104-da1b6568686e
+
+require (
+	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0
+	golang.org/x/image v0.13.0
+)
---
+++ main.go
@@ -0,0 +1,181 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"image"
+	"image/draw"
+	"image/jpeg"
+	"image/png"
+	"net/http"
+	"os"
+
+	"github.com/golang/freetype/truetype"
+	"github.com/skip2/go-qrcode"
+	"golang.org/x/image/font"
+	"golang.org/x/image/math/fixed"
+)
+
+func main() {
+	http.HandleFunc("/view", view)
+	http.HandleFunc("/download", download)
+
+	http.ListenAndServe(":"+*flag.String("port", "8000", "Port server HTTP"), nil)
+	flag.Parse()
+}
+
+func view(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("anda menakses view")
+
+	text := r.FormValue("text")
+	label := r.FormValue("label")
+
+	qr, err := generateQR(text, label)
+	if err != nil {
+		http.Error(w, "Error Generate QR", http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-type", "image/jpeg")
+	jpeg.Encode(w, qr, &jpeg.Options{Quality: 100})
+}
+
+func download(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("anda mengakses download")
+
+	text := r.FormValue("text")
+	label := r.FormValue("label")
+
+	fileName := label + ".jpeg"
+
+	qr, _ := generateQR(text, label)
+
+	w.Header().Set("Content-Disposition", "attachment; filename=smartlink.jpeg")
+	w.Header().Set("Content-Type", "Image/image")
+
+	png.Encode(w, qr)
+	http.ServeFile(w, r, fileName)
+}
+
+func generateQR(text string, label string) (*image.RGBA, error) {
+
+	qrCode, err := qrcode.New(text, qrcode.Medium)
+	if err != nil {
+		return nil, err
+	}
+
+	labelWidth := len(label) * 7
+	labelImg1, _, err_labelImg1 := makeLabelImg(label, labelWidth, 37, "Lato-Black.ttf", 500, "label1")
+	if err_labelImg1 != nil {
+		return nil, err_labelImg1
+	}
+
+	labelImg2, widhtLabel, err_labelImg2 := makeLabelImg("QRIS by NOBU", labelWidth, 37, "Lato-Medium.ttf", 300, "label2")
+	if err_labelImg2 != nil {
+		return nil, err_labelImg2
+	}
+
+	labelImg3, _, err_labelImg3 := makeLabelImg("AIOT Laundry System", labelWidth, 40, "Lato-Black.ttf", 500, "label3")
+	if err_labelImg3 != nil {
+		return nil, err_labelImg3
+	}
+
+	labelImg4, _, err_labelImg4 := makeLabelImg("by Smartlink.id", labelWidth, 40, "Lato-Medium.ttf", 500, "label4")
+	if err_labelImg4 != nil {
+		return nil, err_labelImg4
+	}
+
+	labelImg5, widhtLabel5, err_labelImg5 := makeLabelImg("W01", labelWidth, 100, "Lato-Black.ttf", 500, "label5")
+	if err_labelImg5 != nil {
+		return nil, err_labelImg5
+	}
+
+	importImg, err_importImg := os.Open("washer.png")
+	if err_importImg != nil {
+		return nil, err
+	}
+
+	baseBackground, _, _ := image.Decode(importImg)
+	fmt.Println(baseBackground.Bounds().Dx())
+	fmt.Println(baseBackground.Bounds().Dy())
+	qrImage := qrCode.Image(baseBackground.Bounds().Dx() - 100)
+	qrImageDraw := image.NewRGBA(baseBackground.Bounds())
+
+	x := (baseBackground.Bounds().Dx() - (baseBackground.Bounds().Dx() - 100)) / 2
+	y := (baseBackground.Bounds().Dx() - (baseBackground.Bounds().Dx() - 80)) / 2
+	draw.Draw(qrImageDraw, qrImageDraw.Bounds(), baseBackground, image.Point{}, draw.Over)
+	draw.Draw(qrImageDraw, qrImage.Bounds().Add(image.Pt(x, y)), qrImage, image.Point{}, draw.Over)
+	draw.Draw(qrImageDraw, qrImageDraw.Bounds().Add(image.Pt(100, qrImageDraw.Bounds().Dy()-315)), labelImg1, image.Point{}, draw.Over)
+	draw.Draw(qrImageDraw, qrImageDraw.Bounds().Add(image.Pt(qrImageDraw.Bounds().Dx()-(widhtLabel+100), qrImageDraw.Bounds().Dy()-315)), labelImg2, image.Point{}, draw.Over)
+	draw.Draw(qrImageDraw, qrImageDraw.Bounds().Add(image.Pt(50, qrImageDraw.Bounds().Dy()-170)), labelImg3, image.Point{}, draw.Over)
+	draw.Draw(qrImageDraw, qrImageDraw.Bounds().Add(image.Pt(50, qrImageDraw.Bounds().Dy()-120)), labelImg4, image.Point{}, draw.Over)
+	draw.Draw(qrImageDraw, qrImageDraw.Bounds().Add(image.Pt(baseBackground.Bounds().Dx()-(widhtLabel5+50), (baseBackground.Bounds().Dy()-(widhtLabel5-50)))), labelImg5, image.Point{}, draw.Over)
+	return qrImageDraw, nil
+}
+
+func makeLabelImg(label string, width int, fontSize int, fontFile string, maxWidth int, toLabel string) (*image.RGBA, int, error) {
+	fontFamily, err := callFont(fontFile, float64(fontSize))
+	if err != nil {
+		return nil, 0, err
+	}
+
+	widthLabelSettings := settingWidthLabel(label, fontFamily)
+	fmt.Println("ini widht label setting", widthLabelSettings)
+
+	labelImg := image.NewRGBA(image.Rect(0, 0, maxWidth, fontSize*2))
+	draw.Draw(labelImg, labelImg.Bounds().Add(image.Pt(0, 0)), image.Transparent, image.Point{}, draw.Over)
+
+	if toLabel == "label1" || toLabel == "label2" {
+		d := &font.Drawer{
+			Dst:  labelImg,
+			Src:  image.Black,
+			Face: fontFamily,
+			Dot:  fixed.P(0, fontSize+5),
+		}
+		d.DrawString(label)
+	}
+
+	if toLabel == "label3" || toLabel == "label4" || toLabel == "label5" {
+		d := &font.Drawer{
+			Dst:  labelImg,
+			Src:  image.White,
+			Face: fontFamily,
+			Dot:  fixed.P(0, fontSize+5),
+		}
+		d.DrawString(label)
+	}
+	return labelImg, widthLabelSettings, nil
+}
+
+func callFont(fontFile string, fontSize float64) (font.Face, error) {
+	dataFont, err := os.ReadFile(fontFile)
+	if err != nil {
+		return nil, err
+	}
+
+	fontFormat, err := truetype.Parse(dataFont)
+	if err != nil {
+		return nil, err
+	}
+
+	return truetype.NewFace(fontFormat, &truetype.Options{
+		Size:    fontSize,
+		DPI:     72,
+		Hinting: font.HintingFull,
+	}), nil
+
+}
+
+func settingWidthLabel(label string, face font.Face) int {
+	width := 5
+
+	for _, r := range label {
+		_, advance, ok := face.GlyphBounds(r)
+		if !ok {
+			advance, _ = face.GlyphAdvance(r)
+		}
+		width += int(advance.Ceil())
+	}
+
+	return width
+}
